5caefe151d5382dd8a591337f5a87957defdaab6
seven-october <147933246+seven-october@users.noreply.github.com>
2023-10-14 21:42:05 +0000

    Use CSV

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Deleteing site"
-rm -rf ~/.site 2>/dev/null
+rm -rf .site 2>/dev/null
 
 echo "Creating site"
-mkdir ~/.site
-cp -r ~/assets ~/.site
-cp -r ~/data ~/.site
-cp -r ~/src/* ~/.site
+mkdir .site
+cp -r assets .site
+cp -r data .site
+cp -r src/* .site
